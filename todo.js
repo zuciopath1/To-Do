9ed35e2943b2bdb51d8a670b7fd90b7862876142
zuciopath <73157609+zuciopath@users.noreply.github.com>
2024-07-30 13:29:22 +0400

    Add various JavaScript exercises
    - Implemented a filter exercise to select values less than 3 from an array.
    - Calculated the total price of items in an array using `reduce`.
    - Found the minimum price in an array using `reduce` and `Math.min`.
    - Combined two arrays into one using the spread syntax.
    - Used array destructuring to extract specific elements and the rest of the array.
    - Applied object destructuring to extract and format specific info from an object in an array.

--- todo.js
+++ todo.js
@@ -35,4 +35,61 @@ function addTask(text) {
         taskList.removeChild(li);
     });
 }
+/////////////////////////////////////////////////////////////////////
 
+
+// simple filter exercise
+const someArray = [1,2,3,4,5]
+const newArray = someArray.filter(n=>{
+    return n< 3;
+})
+console.log(`this is filterd array${newArray}`)
+
+
+
+// get total price in the array
+const prices = [29.98, 37.33, 90.21, 30.43]
+const total = prices.reduce( (total,price)=> total+price)
+
+console.log(`total price is ${total}`)
+
+
+// find the minimum price in the array
+const pricess = [29.98, 37.33, 90.21, 30.43, 898.22]
+const minPrice = pricess.reduce((min , price)=>{
+    if(price < min){
+        return price
+    }
+    return min
+})
+console.log(`minimum price is${minPrice}`)
+
+// better option to find the minimum price in the array
+const minNum = Math.min(...pricess)
+console.log('minimum price is:', minNum)
+
+// combine different arrays into new one using spread syntax
+const cats = ['mia','panny','micky']
+const dogs = ['sisi','jany','givi']
+const allPets = [...cats, ...dogs]
+console.log(...allPets)
+
+// destructuring arrays
+const scores = ['lela','sofo','artiom','levani']
+const [first,second, ...evreryoneElse] = scores;
+console.log(first)
+console.log(evreryoneElse)
+
+// object destructuring to extract specific info from the profile array
+const profile = [{
+    firstName:'zura',
+    lastName:'adanaia',
+    dateOfBirth:'1994',
+    bio:'asdjioaj iojoisajdoiji ijiosjdoijdsiofj',
+    workPlace:'curently unemployed'
+}
+]
+const someProfile = profile.map(({firstName, lastName, dateOfBirth})=>{
+    return `${firstName} ${lastName} ${dateOfBirth}`
+})
+console.log(`some info: ${someProfile}`)
